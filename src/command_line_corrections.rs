0740f3f5d911190986eaedbad91fe89ddb0190d3
grzegorz.siekaniec <grzegorz.siekaniec@gmail.com>
2020-06-13 17:34:23 +0200

    refactoring - combining code for command line and REST API

--- src/command_line_corrections.rs
+++ src/command_line_corrections.rs
@@ -3,50 +3,6 @@ use norvig_spell_checker::spell_checker::SpellChecker;
 use rayon::prelude::*;
 use std::time::Instant;
 
-pub fn provide_words_corrections(spell_checker: &SpellChecker, words: Vec<&str>) {
-    info!("Words {:?}", words);
-    let now = Instant::now();
-
-    // let hm_par = sc.word_count.clone();
-    //
-    // let sc = norvig_spell_checker::spell_checker::SpellChecker::from_corpus_file(&corpus_file);
-    // let hm_seq = sc.word_count.clone();
-
-    // assert_eq!(hm_par, hm_seq);
-
-    let word_correction: Vec<Vec<_>> = words
-        .par_iter()
-        .map(|word| {
-            let word = word.to_string();
-            let correction = spell_checker.correction(&word);
-            vec![word, correction]
-        })
-        .collect();
-
-    print_correction(&word_correction);
-
-    let new_now = Instant::now();
-    info!(
-        "It took {:?} to find corrections for words",
-        new_now.duration_since(now)
-    );
-}
-
-fn print_correction(word_correction: &Vec<Vec<String>>) {
-    let mut ascii_table = AsciiTable::default();
-    let mut word_column = Column::default();
-    word_column.header = "Word".into();
-    word_column.align = Align::Left;
-    ascii_table.columns.insert(0, word_column);
-
-    let mut suggestion_column = Column::default();
-    suggestion_column.header = "Correction".into();
-    suggestion_column.align = Align::Left;
-    ascii_table.columns.insert(1, suggestion_column);
-
-    ascii_table.print(word_correction);
-}
-
 #[derive(Debug, Deserialize, Serialize)]
 #[serde(rename_all = "camelCase")]
 pub struct CorrectionRequest {
@@ -56,17 +12,17 @@ pub struct CorrectionRequest {
 #[derive(Serialize)]
 #[serde(rename_all = "camelCase")]
 pub struct Correction {
-    word: String,
-    correction: String,
+    pub word: String,
+    pub correction: String,
 }
 
 #[derive(Serialize)]
 #[serde(rename_all = "camelCase")]
 pub struct CorrectionResponse {
-    corrections: Vec<Correction>,
+    pub corrections: Vec<Correction>,
 }
 // merge with above
-pub fn provide_words_corrections_req(
+pub fn find_words_corrections(
     spell_checker: &SpellChecker,
     words: Vec<String>,
 ) -> CorrectionResponse {
@@ -80,11 +36,25 @@ pub fn provide_words_corrections_req(
             Correction { word, correction }
         })
         .collect();
-
     let new_now = Instant::now();
     info!(
         "It took {:?} to find corrections for words",
         new_now.duration_since(now)
     );
     CorrectionResponse { corrections }
+}
+
+pub fn print_correction(word_correction: &Vec<Vec<String>>) {
+    let mut ascii_table = AsciiTable::default();
+    let mut word_column = Column::default();
+    word_column.header = "Word".into();
+    word_column.align = Align::Left;
+    ascii_table.columns.insert(0, word_column);
+
+    let mut suggestion_column = Column::default();
+    suggestion_column.header = "Correction".into();
+    suggestion_column.align = Align::Left;
+    ascii_table.columns.insert(1, suggestion_column);
+
+    ascii_table.print(word_correction);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,25 @@
 #[macro_use]
 extern crate log;
+extern crate norvig_spell_checker;
 extern crate env_logger;
 extern crate hyper;
-extern crate norvig_spell_checker;
 extern crate rayon;
 #[macro_use]
 extern crate serde_derive;
 extern crate serde_json;
 
 use std::convert::Infallible;
-//use futures::{future, Future};
 use hyper::service::{make_service_fn, service_fn};
-use hyper::{Body, Error, Method, Request, Response, Server, StatusCode};
+use hyper::{Body, Method, Request, Response, Server, StatusCode};
 
 use clap::{App, Arg, ArgMatches, SubCommand};
-use command_line_corrections::provide_words_corrections;
 use dotenv::dotenv;
 use std::env;
-use futures::{StreamExt, TryStreamExt, TryFutureExt};
+use futures::{StreamExt};
 use norvig_spell_checker::spell_checker::SpellChecker;
 use std::sync::Arc;
-use crate::command_line_corrections::{CorrectionResponse, CorrectionRequest, provide_words_corrections_req};
+use crate::command_line_corrections::{CorrectionResponse, CorrectionRequest,
+                                      find_words_corrections, print_correction};
 
 const CMD_RUN: &str = "run";
 const CMD_CORRECT: &str = "correct";
@@ -66,9 +65,15 @@ pub async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>>  {
 
     match matches.subcommand() {
         (CMD_CORRECT, Some(matches)) => {
-            let (corpus_file, words) = correct_cmd_handler(&matches);
+            let (corpus_file, words) = cli_correction_handler(&matches);
             let spell_checker = SpellChecker::from_corpus_file_par(&corpus_file);
-            provide_words_corrections(&spell_checker, words);
+            let word_and_corrections = find_words_corrections(&spell_checker, words);
+            let word_and_correction_vec
+                = word_and_corrections.corrections
+                .into_iter()
+                .map(|correction| vec![correction.word, correction.correction])
+                .collect();
+            print_correction(&word_and_correction_vec);
             Ok(())
         }
         (CMD_RUN, Some(matches)) => {
@@ -110,21 +115,16 @@ pub async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>>  {
     }
 }
 
-// TODO: rename
-fn correct_cmd_handler<'a>(matches: &'a ArgMatches) -> (String, Vec<&'a str>) {
-    let corpus_file: String = {
-        let corpus_arg = matches.value_of("corpus");
-        if corpus_arg.is_some() {
-            corpus_arg.unwrap().to_string()
-        } else {
-            info!("Using default corpus file");
-            String::from("/home/gsiekaniec/devel/rust_projects/norvig_spell_checker/data/big.txt")
-        }
-    };
-    let words: Vec<&str> = {
+fn cli_correction_handler(matches: &ArgMatches) -> (String, Vec<String>) {
+    let corpus_file = corpus_file(matches);
+    let words: Vec<String> = {
         let arg_words = matches.values_of("words");
         if arg_words.is_some() {
-            arg_words.unwrap().collect()
+            arg_words
+                .unwrap()
+                .into_iter()
+                .map(|word| word.to_string())
+                .collect()
         } else {
             vec![]
         }
@@ -146,8 +146,8 @@ fn corpus_file(matches: &ArgMatches) -> String {
     corpus_file
 }
 
-//, spell_checker: &SpellChecker
-async fn microservice_handler(mut req: Request<Body>, spell_checker: Arc<SpellChecker>) -> Result<Response<Body>, Infallible> {
+async fn microservice_handler(mut req: Request<Body>, spell_checker: Arc<SpellChecker>)
+    -> Result<Response<Body>, Infallible> {
 
     match (req.method(), req.uri().path()){
         (&Method::GET, "/correction") => {
@@ -161,15 +161,17 @@ async fn microservice_handler(mut req: Request<Body>, spell_checker: Arc<SpellCh
     }
 }
 
-async fn handle_get_correction_request(req: &mut Request<Body>, spell_checker: Arc<SpellChecker>) -> CorrectionResponse {
+async fn handle_get_correction_request(req: &mut Request<Body>, spell_checker: Arc<SpellChecker>)
+    -> CorrectionResponse {
+
     let mut body = Vec::new();
     while let Some(chunk) = req.body_mut().next().await {
         body.extend_from_slice(&chunk.unwrap());
     }
-    // try to parse as json with serde_json
+    // TODO: add handling in case parsing fails
     let correction_req: CorrectionRequest = serde_json::from_slice(&body).unwrap();
     info!("Received {:?}", correction_req);
-    provide_words_corrections_req(&spell_checker, correction_req.words)
+    find_words_corrections(&spell_checker, correction_req.words)
 }
 
 
